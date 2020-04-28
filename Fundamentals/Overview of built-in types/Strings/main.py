6e45782af2bb6f9b2cc5eab3b5bd4da1eaf85963
Paolo Lammens <lammenspaolo@gmail.com>
2020-04-28 22:30:58 +0200

    feat: theory: strings

--- Fundamentals/Overview of built-in types/Strings/main.py
+++ Fundamentals/Overview of built-in types/Strings/main.py
@@ -1,3 +1,9 @@
-if __name__== "__main__":
-    # Write your solution here
-    pass
+# ----- strings (sequence of characters) ------
+str  # type object ("official" name)
+
+"this is a string"  # can use double quotes
+'this is a string'  # or single quotes
+""  # strings can be empty
+# contrary to other languages, there is no type for individual characters:
+print(type('a'))  # they're just one-character strings
+print("spam eggs")  # you can print strings to the terminal
